ab49dbe911aab585abd9004db09c8b900ee4287d
hodev.dev <hodev.dev@gmail.com>
2022-07-12 17:11:38 +0430

    first

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+var ip = require('ip');
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  res.status(200).json({ name: 'John Doe', ip: ip.address() })
 }
